e2e4e5c664e821b2b51c7c15820a7f8a47fd7197
Dawid Konrad Kohnke <dawid.kohnke.cad@gmail.com>
2025-11-29 16:32:58 +0100

    Move header and footer to js

---
+++ js/header.js
@@ -0,0 +1,21 @@
+document.write(`
+    <!-- Header -->
+    <header id="header" class="alt">
+        <h1><a href="./">WERONIKA JEKA</a></h1>
+        <nav id="nav">
+            <ul>
+                <li class="special">
+                    <a href="#menu" class="menuToggle"><span>Menu</span></a>
+                    <div id="menu">
+                        <ul>
+                            <li><b><a href="./">HOME</a></b></li>
+                            <li><a href="o_mnie.html">O MNIE</a></li>
+                            <li><a href="oferta.html">OFERTA</a></li>
+                            <li><a href="kontakt.html">KONTAKT</a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    </header>
+`);
